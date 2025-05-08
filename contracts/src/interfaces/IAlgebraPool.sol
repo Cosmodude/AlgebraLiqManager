aa2d387de95d426c981457ef934bcaed123521b9
Cosmodude <vladislav.lenskii@gmail.com>
2025-05-08 15:35:57 +0900

    requiored interfaces

---
+++ contracts/src/interfaces/IAlgebraPool.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+// copied from here https://vscode.blockscan.com/80094/0x90f79fdec42351e514c35cd93cb5f1b965585132
+pragma solidity ^0.8.0;
+
+interface IBullaPool {
+    /// @notice Safely get most important state values of Algebra Integral AMM
+    /// @dev Several values exposed as a single method to save gas when accessed externally.
+    /// **Important security note: this method checks reentrancy lock and should be preferred in most cases**.
+    /// @return sqrtPrice The current price of the pool as a sqrt(dToken1/dToken0) Q64.96 value
+    /// @return tick The current global tick of the pool. May not always be equal to SqrtTickMath.getTickAtSqrtRatio(price) if the price is on a tick boundary
+    /// @return lastFee The current (last known) pool fee value in hundredths of a bip, i.e. 1e-6 (so '100' is '0.01%'). May be obsolete if using dynamic fee plugin
+    /// @return pluginConfig The current plugin config as bitmap. Each bit is responsible for enabling/disabling the hooks, the last bit turns on/off dynamic fees logic
+    /// @return activeLiquidity  The currently in-range liquidity available to the pool
+    /// @return nextTick The next initialized tick after current global tick
+    /// @return previousTick The previous initialized tick before (or at) current global tick
+    function safelyGetStateOfAMM()
+        external
+        view
+        returns (uint160 sqrtPrice, int24 tick, uint16 lastFee, uint8 pluginConfig, uint128 activeLiquidity, int24 nextTick, int24 previousTick);
+
+    /// @notice Allows to easily get current reentrancy lock status
+    /// @dev can be used to prevent read-only reentrancy.
+    /// This method just returns `globalState.unlocked` value
+    /// @return unlocked Reentrancy lock flag, true if the pool currently is unlocked, otherwise - false
+    function isUnlocked() external view returns (bool unlocked);
+
+    // ! IMPORTANT security note: the pool state can be manipulated.
+    // ! The following methods do not check reentrancy lock themselves.
+    /// @notice The globalState structure in the pool stores many values but requires only one slot
+    /// and is exposed as a single method to save gas when accessed externally.
+    /// @dev **important security note: caller should check `unlocked` flag to prevent read-only reentrancy**
+    /// @return price The current price of the pool as a sqrt(dToken1/dToken0) Q64.96 value
+    /// @return tick The current tick of the pool, i.e. according to the last tick transition that was run
+    /// This value may not always be equal to SqrtTickMath.getTickAtSqrtRatio(price) if the price is on a tick boundary
+    /// @return lastFee The current (last known) pool fee value in hundredths of a bip, i.e. 1e-6 (so '100' is '0.01%'). May be obsolete if using dynamic fee plugin
+    /// @return pluginConfig The current plugin config as bitmap. Each bit is responsible for enabling/disabling the hooks, the last bit turns on/off dynamic fees logic
+    /// @return communityFee The community fee represented as a percent of all collected fee in thousandths, i.e. 1e-3 (so 100 is 10%)
+    /// @return unlocked Reentrancy lock flag, true if the pool currently is unlocked, otherwise - false
+    function globalState() external view returns (uint160 price, int24 tick, uint16 lastFee, uint8 pluginConfig, uint16 communityFee, bool unlocked);
+
+    /// @notice The first of the two tokens of the pool, sorted by address
+    /// @return The token contract address
+    function token0() external view returns (address);
+
+    /// @notice The second of the two tokens of the pool, sorted by address
+    /// @return The token contract address
+    function token1() external view returns (address);
+
+    /// @notice The current tick spacing
+    /// @dev Ticks can only be initialized by new mints at multiples of this value
+    /// e.g.: a tickSpacing of 60 means ticks can be initialized every 60th tick, i.e., ..., -120, -60, 0, 60, 120, ...
+    /// However, tickspacing can be changed after the ticks have been initialized.
+    /// This value is an int24 to avoid casting even though it is always positive.
+    /// @return The current tick spacing
+    function tickSpacing() external view returns (int24);
+    
+    /// @notice Adds liquidity for the given recipient/bottomTick/topTick position
+    /// @dev The caller of this method receives a callback in the form of IAlgebraMintCallback#algebraMintCallback
+    /// in which they must pay any token0 or token1 owed for the liquidity. The amount of token0/token1 due depends
+    /// on bottomTick, topTick, the amount of liquidity, and the current price.
+    /// @param leftoversRecipient The address which will receive potential surplus of paid tokens
+    /// @param recipient The address for which the liquidity will be created
+    /// @param bottomTick The lower tick of the position in which to add liquidity
+    /// @param topTick The upper tick of the position in which to add liquidity
+    /// @param liquidityDesired The desired amount of liquidity to mint
+    /// @param data Any data that should be passed through to the callback
+    /// @return amount0 The amount of token0 that was paid to mint the given amount of liquidity. Matches the value in the callback
+    /// @return amount1 The amount of token1 that was paid to mint the given amount of liquidity. Matches the value in the callback
+    /// @return liquidityActual The actual minted amount of liquidity
+    function mint(
+        address leftoversRecipient,
+        address recipient,
+        int24 bottomTick,
+        int24 topTick,
+        uint128 liquidityDesired,
+        bytes calldata data
+    ) external returns (uint256 amount0, uint256 amount1, uint128 liquidityActual);
+
+
+    /// @notice Burn liquidity from the sender and account tokens owed for the liquidity to the position
+    /// @dev Can be used to trigger a recalculation of fees owed to a position by calling with an amount of 0
+    /// @dev Fees must be collected separately via a call to #collect
+    /// @param bottomTick The lower tick of the position for which to burn liquidity
+    /// @param topTick The upper tick of the position for which to burn liquidity
+    /// @param amount How much liquidity to burn
+    /// @param data Any data that should be passed through to the plugin
+    /// @return amount0 The amount of token0 sent to the recipient
+    /// @return amount1 The amount of token1 sent to the recipient
+    function burn(int24 bottomTick, int24 topTick, uint128 amount, bytes calldata data) external returns (uint256 amount0, uint256 amount1);
+
+
+    /// @notice Collects tokens owed to a position
+    /// @dev Does not recompute fees earned, which must be done either via mint or burn of any amount of liquidity.
+    /// Collect must be called by the position owner. To withdraw only token0 or only token1, amount0Requested or
+    /// amount1Requested may be set to zero. To withdraw all tokens owed, caller may pass any value greater than the
+    /// actual tokens owed, e.g. type(uint128).max. Tokens owed may be from accumulated swap fees or burned liquidity.
+    /// @param recipient The address which should receive the fees collected
+    /// @param bottomTick The lower tick of the position for which to collect fees
+    /// @param topTick The upper tick of the position for which to collect fees
+    /// @param amount0Requested How much token0 should be withdrawn from the fees owed
+    /// @param amount1Requested How much token1 should be withdrawn from the fees owed
+    /// @return amount0 The amount of fees collected in token0
+    /// @return amount1 The amount of fees collected in token1
+    function collect(
+        address recipient,
+        int24 bottomTick,
+        int24 topTick,
+        uint128 amount0Requested,
+        uint128 amount1Requested
+    ) external returns (uint128 amount0, uint128 amount1);
+
+} 
